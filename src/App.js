aa842caf1231d3e2069ddb93ed530b69d019d493
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:28:41 -0500

    add hello world message

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+<<<<<<< HEAD
           Hello World! This is our team project.
         </p>	
+=======
+          Hello World!
+        </p>
+>>>>>>> 6cb53b9 (add hello world message)
       </header>
     </div>
   );
